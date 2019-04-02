9a843d6dbd9f71dde1da52de931c1c6975cb1d5a
unknown <Karimov Raysultan>
2019-04-02 10:08:11 +0700

    setup added

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {render} from 'react-dom';
+import './index.css';
+import App from './containers/App/App';
+
+render(
+  <App />,
+  document.getElementById('root')
+)
