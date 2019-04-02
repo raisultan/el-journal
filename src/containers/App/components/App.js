6b6b8083b93ff8e53fc79a4793ad4d5a07561a17
Raysultan <ki.xbozz@gmail.com>
2019-04-02 18:16:56 +0700

    layout

--- src/containers/App/components/App.js
+++ src/containers/App/components/App.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react'
-import { Button, Menu } from 'antd'
+import Layout from '../../Layout/components/index'
 import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h2>El journal</h2>  
-        <Button type="primary">Button</Button>
-      </div>
+      <Layout />
     )
   }
 }
