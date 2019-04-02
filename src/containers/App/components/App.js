8a6fb4dc8583f85b85440dc24bed2366c570e5f6
unknown <Karimov Raysultan>
2019-04-02 18:05:28 +0700

    antd added

---
+++ src/containers/App/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { Button, Menu } from 'antd'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>El journal</h2>  
+        <Button type="primary">Button</Button>
+      </div>
+    )
+  }
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import {render} from 'react-dom';
-import './index.css';
-import App from './containers/App/App';
+import ReactDOM from 'react-dom';
+import App from './containers/App/components/App';
 
-render(
-  <App />,
-  document.getElementById('root')
-)
+ReactDOM.render(
+  <App />
+  , document.getElementById('root')
+);
