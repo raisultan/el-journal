acb47f9a64ac20cbd1f100e83f55d87ad1697a54
Raysultan <ki.xbozz@gmail.com>
2019-04-17 15:42:40 +0700

    basic eventCard done

---
+++ client/src/containers/EventsContainer/components/EventCard.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Card, Icon } from 'antd'
+
+export default ({title, desc, date}) => {
+
+  const { Meta } = Card
+  const actions = [
+    <span>Изменить <Icon type="edit" /></span>,
+    <span>Подробнее <Icon type="ellipsis" /></span>
+  ]
+
+  return (
+    <Card
+      style={{ width: '100%' }}
+      actions={actions}
+    >
+      <Meta
+        title={title}
+        description={desc}
+      />
+      <br />
+      {date}
+    </Card>
+  )
+}
--- client/src/containers/EventsContainer/index.js
+++ client/src/containers/EventsContainer/index.js
@@ -1,9 +1,47 @@
 import React from 'react'
 
+import EventCard from './components/EventCard'
+import { EventsWrapper } from './styled'
+
+const events = [
+  {
+    title: 'Событие №1',
+    desc: 'Описание события...',
+    date: '12.09.2019 - 15.09.2019'
+  },
+  {
+    title: 'Событие №2',
+    desc: 'Описание события...',
+    date: '12.09.2019 - 15.09.2019'
+  },
+  {
+    title: 'Событие №3',
+    desc: 'Описание события...',
+    date: '12.09.2019 - 15.09.2019'
+  },
+  {
+    title: 'Событие №4',
+    desc: 'Описание события...',
+    date: '12.09.2019 - 15.09.2019'
+  },
+]
+
 export default () => {
+
+  const cards = (
+    events.map((event) => (
+      <EventCard
+        key={event.title}
+        title={event.title}
+        desc={event.desc}
+        date={event.date}
+      />
+    ))
+  )
+
   return (
-    <>
-      <h2>Events</h2>
-    </>
+    <EventsWrapper>
+      {cards}
+    </ EventsWrapper>
   )
 }
