3bb73bf7b7a86565105734e5c78262e702d55c6f
Raysultan Karimov <ki.xbozz@gmail.com>
2019-05-22 05:43:55 +0700

    basic account page done

---
+++ client/src/containers/AccountContainer/components/AccountCard.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Typography } from 'antd'
+
+import { CenterBlock, StyledAccountCard } from '../styled';
+
+const { Text } = Typography
+
+export default ({ user }) => {
+  const { name, 
+    surname, 
+    lastname, 
+    birth, 
+    address, 
+    phone} = user
+  return (
+    <CenterBlock>
+      <StyledAccountCard>
+        <Text>Фамилия: {surname}</Text> <br />
+        <Text>Имя: {name}</Text> <br />
+        <Text>Отчество: {lastname}</Text> <br />
+        <Text>Дата рождения: {birth}</Text> <br />
+        <Text>Адрес: {address}</Text> <br />
+        <Text>Телефон: {phone}</Text> <br />
+      </StyledAccountCard>
+    </CenterBlock>
+  )
+}
--- client/src/containers/AccountContainer/index.js
+++ client/src/containers/AccountContainer/index.js
@@ -1,7 +1,18 @@
 import React from 'react'
 
+import AccountCard from './components/AccountCard'
+
+const user = {
+  name: 'Антон',
+  surname: 'Забалотный',
+  lastname: 'Генадьевич',
+  birth: '09.04.1989',
+  address: 'Красноярск, ул. Киренского 78б, кв. 31',
+  phone: '+79618913285',
+}
+
 export default () => {
   return (
-    <h2>Hello, my name is User</h2>
+    <AccountCard user={user}/>
   )
 }
