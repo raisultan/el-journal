4f192a4024a2c1a91587ffaca95dfc0d9b4e5e64
Raysultan <ki.xbozz@gmail.com>
2019-10-09 22:47:50 +0700

    reset server

--- server/routes/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-const authController = require('../api/controllers/auth');
-/* GET home page. */
-router.post('/auth/login', authController.signIn);
-
-module.exports = router;
